742bc41f77f0265f42bdade924382507eb68f27d
PawelGolik <golikpawel@o2.pl>
2019-11-14 22:05:01 +0100

    Changed back to x64
        Changed time to chrono lib.
        changed magic variables to const's.

--- TetrisExample/Main.cpp
+++ TetrisExample/Main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <time.h>
-#include <cstdlib>
+#include <SFML/Window.hpp>
+#include <random>
+#include <vector>
+#include <chrono>
 
+#define CUBE_SIZE 8
 #define WIDTH 1000
 #define HEIGHT 600
 
 using namespace sf;
+
+auto randF = [](int min_val, int max_val){
+			std::mt19937_64 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+			return std::uniform_int_distribution<int>(min_val,max_val)(gen);
+		};
 
 enum MOVMENT
 {
@@ -20,38 +28,36 @@ enum PIECE
 
 std::vector<Sprite> ReturnPiece(PIECE piece,Texture &tiles) {
 	std::vector<Sprite> outputVector;
-	std::srand(10);
-	int row = (rand() % 9) * 8;
-	std::srand(10000);
-	int col = (rand() % 3) * 8;
+	int row = randF(0,9) * CUBE_SIZE;
+	int col = randF(0,3) * CUBE_SIZE;
 	for (size_t i = 0; i < 4; i++)
 	{
 		Sprite cube;
 		cube.setTexture(tiles);
-		cube.setTextureRect({ col,row,8,8 });
+		cube.setTextureRect({ col,row,CUBE_SIZE,CUBE_SIZE });
 		cube.setPosition(WIDTH / 2, HEIGHT / 2);
 		outputVector.push_back(cube);
 	}
 	if (piece == PIECE::RANDOM) {
-		piece = PIECE(rand() % 3);
+		piece = PIECE(randF(0,3));
 	}
 	switch (piece)
 	{
 	case PIECE::CUBE:
-		outputVector[0].setOrigin(8,8);
-		outputVector[1].setOrigin(0, 8);
-		outputVector[2].setOrigin(8, 0);
+		outputVector[0].setOrigin(CUBE_SIZE,CUBE_SIZE);
+		outputVector[1].setOrigin(0, CUBE_SIZE);
+		outputVector[2].setOrigin(CUBE_SIZE, 0);
 		outputVector[3].setOrigin(0, 0);
 		break;
 	case PIECE::LONG:
 		outputVector[0].setOrigin(4, 16);
-		outputVector[1].setOrigin(4, 8);
+		outputVector[1].setOrigin(4, CUBE_SIZE);
 		outputVector[2].setOrigin(4, 0);
-		outputVector[3].setOrigin(4, -8);
+		outputVector[3].setOrigin(4, -CUBE_SIZE);
 		break;
 	case PIECE::SLIKE:
-		outputVector[0].setOrigin(12, 8);
-		outputVector[1].setOrigin(4, 8);
+		outputVector[0].setOrigin(12, CUBE_SIZE);
+		outputVector[1].setOrigin(4, CUBE_SIZE);
 		outputVector[2].setOrigin(4, 0);
 		outputVector[3].setOrigin(-4, 0);
 		break;
@@ -97,10 +103,8 @@ void Movement(MOVMENT move, std::vector<Sprite> &draw) {
 
 int main() {
 
-	std::srand(NULL);
-	int row = (rand() % 9) * 8;
-	std::srand(NULL);
-	int col = (rand() % 3) * 8;
+	int row = randF(0,9) * 8;
+	int col = randF(0,3) * 8;
 	VideoMode vm(WIDTH, HEIGHT);
 	Texture texture;
 	texture.loadFromFile("Tails.png");
@@ -109,7 +113,7 @@ int main() {
 	cube.setTexture(texture);
 	cube.setTextureRect(IntRect(col, row, 8, 8));
 	cube.setPosition(WIDTH / 2, HEIGHT / 2);
-	Draw.push_back({ReturnPiece(PIECE::CUBE, texture), true});
+	Draw.push_back({ReturnPiece(PIECE::RANDOM, texture), true});
 	RenderWindow window(vm, "Tetris");
 	View view;
 	view.setSize(Vector2f(500, 500));
